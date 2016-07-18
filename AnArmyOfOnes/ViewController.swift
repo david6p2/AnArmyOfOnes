a268734625bcfc620fcf0f2d8da5fd00151d2cbc
David Céspedes <ingdavidcespedes@gmail.com>
2016-07-17 20:34:17 -0500

    Initial Commit

---
+++ AnArmyOfOnes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnArmyOfOnes
+//
+//  Created by David Céspedes on 17/07/16.
+//  Copyright © 2016 LSR Marketing Service. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
